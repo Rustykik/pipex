c5c84d968ed2be405fe75df45f14ba640d63227e
Rustykik <valentin.dubr@gmail.com>
2022-01-23 04:25:09 +0300

    invalid argument fixed2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rusty <rusty@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 04:08:45 by rusty             #+#    #+#             */
-/*   Updated: 2022/01/23 04:10:18 by rusty            ###   ########.fr       */
+/*   Updated: 2022/01/23 04:24:01 by rusty            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	*pipex_all_info;
 
-	if ((argc <= 5 && ft_strncmp(argv[1], "here_doc", 9)) || argc <= 6)
+	if (!((argc >= 5 && ft_strncmp(argv[1], "here_doc", 9)) || argc >= 6))
 	{
-		ft_printf("Invalid arguments\n");
+		ft_putstr_fd("Invalid arguments\n", 2);
 		exit(-1);
 	}
 	pipex_all_info = (t_pipex *)ft_calloc(sizeof(t_pipex), 1);
